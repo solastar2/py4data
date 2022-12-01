49e2885b816ef73003a75587f15c383b0ff01f20
solastar2 <solastar2@yahoo.com>
2022-12-01 15:34:46 -0600

    Add files via upload

---
+++ Jupyter_Notebook.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo"  />
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Saubana Olorunsola Dada** 
+# Research Assistant
+
+# *I am interested in data science because I see it as the future. Also, my research work requires knowledge of data science.*
+
+# ### My code is suppose to predict collagen concentration in the body###
+
+# In[1]:
+
+
+1+1
+
+
+# horizontal rule...
+# ---
+# are drawn
+# ***
+# Let,s | try | a | table
+# ~~strikethrough.~~
+
+# In[ ]:
+
+
+
+
